ba64713545cecdaff06cbc2d56c2f0d2cd9077a2
albelyaev <alexandr.belyaev@gmail.com>
2017-01-19 16:14:27 +0300

    Main program with no logging

--- Decorator/Decorator/Program.cs
+++ Decorator/Decorator/Program.cs
@@ -10,6 +10,23 @@ namespace Decorator
 	{
 		static void Main( string[] args )
 		{
+			Console.WriteLine( "***\r\n Begin program - no logging\r\n" );
+
+			IRepository<Customer> customerRepository = new Repository<Customer>();
+
+			var customer = new Customer
+			{
+				Id = 1,
+				Name = "Customer 1",
+				Address = "Address 1"
+			};
+
+			customerRepository.Add(customer);
+			customerRepository.Update(customer);
+			customerRepository.Delete(customer);
+
+			Console.WriteLine( "\r\nEnd program - no logging\r\n***" );
+			Console.ReadKey();
 		}
 	}
 }
