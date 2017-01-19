5d965c1fa0aaaed57ce75e0f1a1e09ad93308864
albelyaev <alexandr.belyaev@gmail.com>
2017-01-19 16:03:20 +0300

    IRepository

---
+++ Decorator/Decorator/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+	public interface IRepository<T>
+	{
+		void Add( T entity );
+
+		void Delete( T entity );
+
+		void Update( T entity );
+
+		IEnumerable<T> GetAll();
+
+		T GetById( int id );
+	}
+}
